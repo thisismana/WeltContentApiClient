a066ba9852af2b986f41b631520a5c0bf8e8d1ee
harryurban <harald.urban@axelspringer.de>
2016-11-15 16:51:19 +0100

    add extra Reads and Writes for ApiSection

--- core/src/main/scala/de/welt/contentapi/core/models/section/SectionPageFormats.scala
+++ core/src/main/scala/de/welt/contentapi/core/models/section/SectionPageFormats.scala
@@ -15,3 +15,27 @@ object SectionPageFormats {
   implicit val stageFormat = Json.format[Stage]
   implicit val sectionPageFormat = Json.format[ApiSection]
 }
+
+object SectionPageWrites {
+
+  import de.welt.contentapi.core.models.content.ApiContentWrites._
+
+  implicit val apiCommercialWrite = Json.writes[ApiCommercial]
+
+  implicit val teaserConfigWrite = Json.writes[TeaserConfig]
+  implicit val teaserWrite = Json.writes[Teaser]
+  implicit val stageWrite = Json.writes[Stage]
+  implicit val sectionPageWrite = Json.writes[ApiSection]
+}
+
+object SectionPageReads {
+
+  import de.welt.contentapi.core.models.content.ApiContentReads._
+
+  implicit val apiCommercialRead = Json.reads[ApiCommercial]
+
+  implicit val teaserConfigRead = Json.reads[TeaserConfig]
+  implicit val teaserRead = Json.reads[Teaser]
+  implicit val stageRead = Json.reads[Stage]
+  implicit val sectionPageRead = Json.reads[ApiSection]
+}
