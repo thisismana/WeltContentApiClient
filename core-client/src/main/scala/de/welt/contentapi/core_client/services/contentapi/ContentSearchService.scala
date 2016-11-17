6701d3d82ba68bf95d2c2363be0f2fe8572a9ec7
harryurban <harald.urban@axelspringer.de>
2016-11-17 16:46:48 +0100

    only delivers ApiContent now - use ContentEnrichmentService!

--- core-client/src/main/scala/de/welt/contentapi/core_client/services/contentapi/ContentSearchService.scala
+++ core-client/src/main/scala/de/welt/contentapi/core_client/services/contentapi/ContentSearchService.scala
@@ -3,10 +3,10 @@ package de.welt.contentapi.core_client.services.contentapi
 import javax.inject.{Inject, Singleton}
 
 import com.kenshoo.play.metrics.Metrics
+import de.welt.contentapi.core.models.ApiContent
 import de.welt.contentapi.core_client.models.ApiContentSearch
 import de.welt.contentapi.core_client.services.configuration.{ContentClientConfig, ServiceConfiguration}
-import de.welt.contentapi.core.models.content.ApiContent
-import de.welt.contentapi.core.models.internal.http.RequestHeaders
+import de.welt.contentapi.core_client.services.http.RequestHeaders
 import play.api.libs.json.{JsLookupResult, JsResult}
 import play.api.libs.ws.WSClient
 
@@ -16,7 +16,7 @@ sealed trait ContentSearchService {
   protected val serviceName = "search"
 
   def search(query: ApiContentSearch)
-            (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext): Future[Seq[ApiEnrichedContent]]
+            (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext): Future[Seq[ApiContent]]
 }
 
 @Singleton
@@ -31,14 +31,10 @@ class ContentSearchServiceImpl @Inject()(override val ws: WSClient,
   override def jsonValidate: (JsLookupResult) => JsResult[Seq[ApiContent]] = json => json.validate[Seq[ApiContent]]
 
   override def search(apiContentSearch: ApiContentSearch)
-                     (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext): Future[Seq[ApiEnrichedContent]] = {
+                     (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext): Future[Seq[ApiContent]] = {
 
 
-    get(Nil, apiContentSearch.getAllParamsUnwrapped, Nil).map { responses ⇒
-      responses.map { content ⇒
-        sectionService.enrich(content)
-      }
-    }
+    get(Nil, apiContentSearch.getAllParamsUnwrapped, Nil)
   }
 
 }
