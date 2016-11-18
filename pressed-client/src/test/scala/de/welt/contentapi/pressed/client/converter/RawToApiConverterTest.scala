59b5865e8251ed5026463e3f798d72238abe6a69
harryurban <harald.urban@axelspringer.de>
2016-11-18 16:49:39 +0100

    Clean and Add Converter Tests

--- pressed-client/src/test/scala/de/welt/contentapi/pressed/client/converter/RawToApiConverterTest.scala
+++ pressed-client/src/test/scala/de/welt/contentapi/pressed/client/converter/RawToApiConverterTest.scala
@@ -1,5 +1,6 @@
 package de.welt.contentapi.pressed.client.converter
 
+import de.welt.contentapi.pressed.models.ApiThemeConfiguration
 import de.welt.contentapi.raw.models._
 import org.scalatest.words.MustVerb
 import org.scalatest.{FlatSpec, Matchers}
@@ -8,17 +9,24 @@ class RawToApiConverterTest extends FlatSpec with Matchers with TestScope with M
 
   val converter: RawToApiConverter = new RawToApiConverter()
 
-  "Converter" should "calculate 'sport' as adTag" in {
+  "AdTag Converter" should "calculate 'sport' as AdTag as the first in line with 'definesAdTag == true'" in {
     converter.calculateAdTag(homeSportFussball) shouldBe homeSport.id.path.replaceAll("/", "")
   }
-
-  it should "calculate 'sport' as videoAdTech" in {
+  it should "calculate 'sport' as VideoAdTag as the first in line with 'definesVideoAdTag == true'" in {
     converter.calculateVideoAdTag(homeSportFussball) shouldBe homeSport.id.path.replaceAll("/", "")
   }
-
-  it should "get adTech 'sonstiges' if no `rootLevel` section has definesAdTech==true" in {
-    // create Channel
+  it should "calculate 'sonstiges' AdTag because no `rootLevel` section has 'definesAdTag == true'" in {
+    converter.calculateAdTag(homePolitikAusland) shouldBe "sonstiges"
   }
+  it should "calculate 'sonstiges' as VideoAdTag because no `rootLevel` section has 'definesVideoAdTag == true'" in {
+    converter.calculateVideoAdTag(homePolitikAusland) shouldBe "sonstiges"
+  }
+
+  "Theme Converter" should "always calculate `default` as Theme, because there is no implementation yet" in {
+    converter.apiThemeFromRawChannel(homeSportFussball) shouldBe ApiThemeConfiguration(name = Some("default"))
+  }
+
+
 
 
 
@@ -84,4 +92,33 @@ trait TestScope {
       )
     )
   )
+
+
+  val homePolitik: RawChannel = RawChannel(
+    RawChannelId(
+      path = "/politik/",
+      label = "Politik"
+    ),
+    parent = Some(home),
+    children = None,
+    config = Some(RawChannelConfiguration(commercial = Some(falseRawChannelCommercial)))
+  )
+  val homePolitikAusland: RawChannel = RawChannel(
+    RawChannelId(
+      path = "/sport/ausland/",
+      label = "Ausland"
+    ),
+    parent = Some(homePolitik),
+    children = None,
+    config = Some(
+      RawChannelConfiguration(
+        commercial = Some(falseRawChannelCommercial),
+        metadata = Some(rawChannelMetaTags),
+        header = Some(fussballRawChannelHeader)
+      )
+    )
+  )
+  
+  
+  
 }
