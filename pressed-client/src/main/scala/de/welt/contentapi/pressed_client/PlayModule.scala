0aaea89aa9c03cf23f8311813af1a17ebddedb5c
harryurban <harald.urban@axelspringer.de>
2016-11-18 13:14:27 +0100

    delete obsolete

--- pressed-client/src/main/scala/de/welt/contentapi/pressed_client/PlayModule.scala
+++
@@ -1,9 +0,0 @@
-package de.welt.contentapi.pressed_client
-
-import play.api.{Configuration, Environment}
-import play.api.inject.{Binding, Module}
-
-class PlayModule extends Module {
-  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] =
-    Seq()
-}
