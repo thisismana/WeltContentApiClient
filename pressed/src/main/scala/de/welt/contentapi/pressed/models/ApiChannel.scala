f83802566777914165c4b716dba880af0edc51a2
bobaaaaa <patrick.dahms@weltn24.de>
2016-11-21 13:07:59 +0100

    remove package visibility

--- pressed/src/main/scala/de/welt/contentapi/pressed/models/ApiChannel.scala
+++ pressed/src/main/scala/de/welt/contentapi/pressed/models/ApiChannel.scala
@@ -7,6 +7,6 @@ import de.welt.contentapi.core.models.ApiReference
   * @param section the current section itself as ApiSectionReference
   * @param breadcrumb the current section and all parents until the 'frontpage' aka '/' aka 'home' as a Seq of ApiSectionReferences
   */
-private[pressed] case class ApiChannel(section: Option[ApiReference] = None, breadcrumb: Option[Seq[ApiReference]] = None) {
+case class ApiChannel(section: Option[ApiReference] = None, breadcrumb: Option[Seq[ApiReference]] = None) {
   lazy val unwrappedBreadcrumb: Seq[ApiReference] = breadcrumb.getOrElse(Nil)
 }
