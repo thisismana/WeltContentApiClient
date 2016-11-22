58365f65b246d2a061b81019cdeb53d0e1b53bea
harryurban <harald.urban@axelspringer.de>
2016-11-22 14:28:02 +0100

    allow Enrichment via 'preview' CMCF tree by passing an Env (but with default = Live)

--- pressed-client/src/main/scala/de/welt/contentapi/pressed/client/services/PressedContentService.scala
+++ pressed-client/src/main/scala/de/welt/contentapi/pressed/client/services/PressedContentService.scala
@@ -9,7 +9,7 @@ import de.welt.contentapi.pressed.client.converter.RawToApiConverter
 import de.welt.contentapi.pressed.models.{ApiChannel, ApiConfiguration, ApiPressedContent}
 import de.welt.contentapi.raw.models.RawChannel
 import de.welt.contentapi.raw_client.services.RawTreeService
-import de.welt.contentapi.utils.Env.Live
+import de.welt.contentapi.utils.Env.{Env, Live}
 import de.welt.contentapi.utils.Loggable
 
 import scala.collection.immutable.Seq
@@ -17,9 +17,9 @@ import scala.concurrent.{ExecutionContext, Future}
 
 trait PressedContentService {
   def find(id: String, showRelated: Boolean = true)
-          (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext): Future[ApiPressedContent]
+          (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext, env: Env = Live): Future[ApiPressedContent]
 
-  def convert(apiContent: ApiContent, related: Option[Seq[ApiContent]] = None): ApiPressedContent
+  def convert(apiContent: ApiContent, related: Option[Seq[ApiContent]] = None, env: Env = Live): ApiPressedContent
 }
 
 @Singleton
@@ -27,14 +27,13 @@ class PressedContentServiceImpl @Inject()(contentService: ContentService, conver
   extends PressedContentService with Loggable {
 
   override def find(id: String, showRelated: Boolean = true)
-                   (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext): Future[ApiPressedContent] =
+                   (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext, env: Env = Live): Future[ApiPressedContent] =
     contentService
       .find(id, showRelated)
-      .map(response ⇒ convert(response.content, response.related))
+      .map(response ⇒ convert(response.content, response.related, env))
 
-  override def convert(apiContent: ApiContent, maybeRelatedContent: Option[Seq[ApiContent]] = None): ApiPressedContent = {
-    // todo (harry): check env-awareness?
-    rawTreeService.root(Live).map { rawTree =>
+  override def convert(apiContent: ApiContent, maybeRelatedContent: Option[Seq[ApiContent]] = None, env: Env = Live): ApiPressedContent = {
+    rawTreeService.root(env).map { rawTree =>
       val maybeRawChannel: Option[RawChannel] = apiContent
         .sections
         .flatMap(_.home)
