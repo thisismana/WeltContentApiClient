19e165d3fbaad9930c1fcf07b8ac9ff8dd14b7a0
harryurban <harald.urban@axelspringer.de>
2016-11-23 15:54:55 +0100

    Dont use ContentClientConfig for S3 configs

--- pressed-client/src/main/scala/de/welt/contentapi/pressed/client/repository/PressedS3Client.scala
+++ pressed-client/src/main/scala/de/welt/contentapi/pressed/client/repository/PressedS3Client.scala
@@ -3,20 +3,22 @@ package de.welt.contentapi.pressed.client.repository
 import java.time.Instant
 import javax.inject.Inject
 
-import de.welt.contentapi.core.client.services.configuration.ContentClientConfig
 import de.welt.contentapi.core.client.services.s3.S3Client
 import de.welt.contentapi.pressed.models.{ApiPressedSection, PressedReads}
 import de.welt.contentapi.utils.Loggable
+import play.api.Configuration
 import play.api.libs.json.{JsError, JsSuccess, Json}
 
 sealed trait PressedS3Client {
   def find(path: String): Option[(ApiPressedSection, Instant)]
 }
 
-case class PressedS3ClientImpl @Inject()(s3Client: S3Client, contentClientConfig: ContentClientConfig) extends PressedS3Client with Loggable {
 
-  val bucket = contentClientConfig.aws.s3.pressed.bucket
-  val file = contentClientConfig.aws.s3.pressed.file
+case class PressedS3ClientImpl @Inject()(s3Client: S3Client, config: Configuration) extends PressedS3Client with Loggable {
+
+  import PressedS3ClientImpl._
+  val bucket = config.getString(bucketConfigKey).getOrElse(throw config.reportError(bucketConfigKey, "Missing Configuration value"))
+  val file = config.getString(fileConfigKey).getOrElse(throw config.reportError(fileConfigKey, "Missing Configuration value"))
 
   override def find(path: String): Option[(ApiPressedSection, Instant)] = {
 
@@ -32,6 +34,10 @@ case class PressedS3ClientImpl @Inject()(s3Client: S3Client, contentClientConfig
             None
         }
     }
-
   }
+}
+
+object PressedS3ClientImpl {
+  val bucketConfigKey = "welt.aws.s3.pressed.bucket"
+  val fileConfigKey = "welt.aws.s3.pressed.file"
 }
