e71994a3784db8b28ebb695940cd11a742fcbfee
harryurban <harald.urban@axelspringer.de>
2016-11-23 16:04:14 +0100

    implicit requestHeaders + executionContext

--- pressed-client/src/main/scala/de/welt/contentapi/pressed/client/repository/PressedDiggerClient.scala
+++ pressed-client/src/main/scala/de/welt/contentapi/pressed/client/repository/PressedDiggerClient.scala
@@ -5,6 +5,7 @@ import javax.inject.Inject
 import com.kenshoo.play.metrics.Metrics
 import de.welt.contentapi.core.client.services.configuration.{ContentClientConfig, ServiceConfiguration}
 import de.welt.contentapi.core.client.services.contentapi.AbstractService
+import de.welt.contentapi.core.client.services.http._
 import de.welt.contentapi.pressed.models.ApiPressedSection
 import de.welt.contentapi.utils.Env.{Env, Live}
 import play.api.libs.json.{JsLookupResult, JsResult}
@@ -14,13 +15,13 @@ import scala.concurrent.{ExecutionContext, Future}
 
 sealed trait PressedDiggerClient {
   protected val serviceName = "digger"
-  def findByPath(path: String, env: Env = Live): Future[ApiPressedSection]
+  def findByPath(path: String, env: Env = Live)
+                (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext): Future[ApiPressedSection]
 }
 
 case class PressedDiggerClientImpl @Inject()(override val ws: WSClient,
                                              override val metrics: Metrics,
-                                             cfg: ContentClientConfig,
-                                             implicit val executionContext: ExecutionContext) extends AbstractService[ApiPressedSection] with PressedDiggerClient {
+                                             cfg: ContentClientConfig) extends AbstractService[ApiPressedSection] with PressedDiggerClient {
 
   override def config: ServiceConfiguration = cfg.getServiceConfig(serviceName)
 
@@ -29,8 +30,10 @@ case class PressedDiggerClientImpl @Inject()(override val ws: WSClient,
   override def jsonValidate: (JsLookupResult) ⇒ JsResult[ApiPressedSection] = jsLookupResult ⇒
     jsLookupResult.validate[ApiPressedSection](apiPressedSectionReads)
 
-  override def findByPath(path: String, env: Env = Live): Future[ApiPressedSection] = {
-      // todo (harry): use env in path for digger, provide formatable 'endpoint'
+  override def findByPath(path: String, env: Env = Live)
+                         (implicit requestHeaders: Option[RequestHeaders], executionContext: ExecutionContext): Future[ApiPressedSection] = {
+      // todo (harry)(digger): use env in path, provide formatable 'endpoint'
+     // /user/env/my/foo/bar/path/
       get(ids = Seq(env.toString, path), parameters = Nil, Nil)
   }
 
