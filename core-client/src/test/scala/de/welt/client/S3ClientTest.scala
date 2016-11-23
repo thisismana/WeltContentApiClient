b484a533ea93ec3c85fec5fc3aa000677297be92
thisismana <matthias.naber@weltn24.de>
2016-11-23 11:31:21 +0100

    fixed tests

--- core-client/src/test/scala/de/welt/client/S3ClientTest.scala
+++ core-client/src/test/scala/de/welt/client/S3ClientTest.scala
@@ -12,26 +12,20 @@ class S3ClientTest extends PlaySpec {
   val prodEnv = Environment(new File("."), getClass.getClassLoader, Mode.Prod)
   val devEnv = Environment(new File("."), getClass.getClassLoader, Mode.Dev)
 
-  val s3Config: Configuration = Configuration(
-    "welt.aws.s3.endpoint" -> "s3.eu-central-1.amazonaws.com"
-  )
-
   class FunkConfigTestImpl(override val configuration: Configuration) extends ContentClientConfigImpl(configuration)
 
   "S3" should {
 
     "return None if s3 is not enabled in config" in {
 
-      val config = new FunkConfigTestImpl(s3Config)
-      val s3 = new S3ClientImpl(config = config)
+      val s3 = new S3ClientImpl(config = Configuration("welt.aws.s3.endpoint" -> "s3.eu-central-1.amazonaws.com"))
 
       s3.client mustBe defined
     }
 
     "return None if there is no s3 endpoint set" in {
 
-      val config = new FunkConfigTestImpl(Configuration())
-      val s3 = new S3ClientImpl(config = config)
+      val s3 = new S3ClientImpl(config = Configuration())
 
       s3.client mustBe None
     }
