f040a7e094f791fb9937a4f02b0a73d8d801b81b
harryurban <harald.urban@axelspringer.de>
2016-11-25 17:39:44 +0100

    bind core module to pressed

--- pressed-client/src/main/scala/de/welt/contentapi/pressed/client/PressedModule.scala
+++ pressed-client/src/main/scala/de/welt/contentapi/pressed/client/PressedModule.scala
@@ -6,12 +6,14 @@ import play.api.{Configuration, Environment}
 import play.api.inject.{Binding, Module}
 
 class PressedModule extends Module {
-  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] =
-    Seq(
+  override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
+    val client = new de.welt.contentapi.core.client.CoreModule()
+    client.bindings(environment, configuration) ++ Seq(
       bind(classOf[PressedS3Client]).to(classOf[PressedS3ClientImpl]),
       bind(classOf[PressedDiggerClient]).to(classOf[PressedDiggerClientImpl]),
 
       bind(classOf[PressedContentService]).to(classOf[PressedContentServiceImpl]),
       bind(classOf[PressedSectionService]).to(classOf[PressedSectionServiceImpl])
     )
+  }
 }
